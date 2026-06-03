c3c38af89f076df95979008756fa44c36420cddf
Salman Miftahur Rizki <salmanmiftahurrizki@192.168.1.4>
2026-06-03 21:54:19 +0700

    security: remove hardcoded fallback midtrans keys to resolve security alert

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'server_key' => env('MIDTRANS_SERVER_KEY', 'SB-Mid-server-xWzF2p_yR_lQ2eLq28H9vR9T'), // Dummy sandbox key
-    'client_key' => env('MIDTRANS_CLIENT_KEY', 'SB-Mid-client-r2E_sBw_L2v_2v2v'), // Dummy sandbox key
+    'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'is_sanitized' => true,
     'is_3ds' => true,
