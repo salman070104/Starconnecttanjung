b2fe242dc2ed8090344b2061528544ffd17d97ab
Salman Miftahur Rizki <salmanmiftahurrizki@192.168.1.26>
2026-07-16 10:32:51 +0700

    Fix IDE warnings in deploy workflow and GoogleAuthController

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -15,7 +15,9 @@ class GoogleAuthController extends Controller
      */
     public function redirect()
     {
-        return Socialite::driver('google')->with(['prompt' => 'select_account'])->redirect();
+        /** @var \Laravel\Socialite\Two\AbstractProvider $driver */
+        $driver = Socialite::driver('google');
+        return $driver->with(['prompt' => 'select_account'])->redirect();
     }
 
     /**
